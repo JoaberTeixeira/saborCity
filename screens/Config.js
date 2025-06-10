9b0050ca8ef8072196fdaaac93b21184ad142c7a
joaber81 <joaberdasilva81@gmail.com>
2025-06-10 18:00:17 -0300

    att

--- screens/Config.js
+++ screens/Config.js
@@ -4,7 +4,7 @@ import styles from "../styles";
 export default function Config() {
   return (
     <View style={styles.container}>
-      <Text>Bem vindos ao 6 info</Text>
+      <Text>Nenhuma configuração disponivel</Text>
     </View>
   );
 }
